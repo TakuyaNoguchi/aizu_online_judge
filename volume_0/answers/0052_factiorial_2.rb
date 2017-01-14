3b0d40fd3eb043904c514ff4b38a799b9235dd80
Takuya Noguchi <takuya@netlab.jp>
2017-01-14 17:38:05 +0900

    3項演算子を使用するように変更。

--- volume_0/answers/0052_factiorial_2.rb
+++ volume_0/answers/0052_factiorial_2.rb
@@ -4,9 +4,5 @@ loop do
 
   matched = [*2..n].inject(&:*).to_s.scan(/[1-9](0+)\Z/).flatten
 
-  if matched[0]
-    puts matched[0].length
-  else
-    puts 0
-  end
+  puts matched[0] ? matched[0].length : 0
 end
